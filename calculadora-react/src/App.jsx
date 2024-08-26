ce29fa61b4da730055dafb29e41b573a0f659eeb
caiomarcelo <caio.marcelo79@gmail.com>
2024-08-25 21:50:00 -0300

    parte 5

--- calculadora-react/src/App.jsx
+++ calculadora-react/src/App.jsx
@@ -8,13 +8,47 @@ const App = () => {
 
 
   const [currentNumber, setCurrentNumber] = useState(0)
-
+  const [firstNumber, setFirstNumber] = useState('0')
+  const [operation, setOperation] = useState('');
 
   const handleClear = () => {
-    setCurrentNumber(0)
+    setCurrentNumber('0')
+    setFirstNumber('0')
+    setOperation('')
   }
   const handleAddNumber = (num) => {
-    setCurrentNumber(prev => `${num}${prev == '0' ? '' : prev}`)
+    setCurrentNumber(prev => `${prev == '0' ? '' : prev}${num}`)
+  }
+
+  const handleSumNumbers = () => {
+
+    if (firstNumber == "0") {
+      setFirstNumber(String(currentNumber))
+      setCurrentNumber('0')
+      setOperation('+')
+    }else{
+      const sum = Number(firstNumber) + Number(currentNumber)
+      setCurrentNumber(String(sum))
+      setOperation('')
+      setFirstNumber('0')
+    }
+
+
+  }
+
+  const handleEquals = () => {
+
+    if (!firstNumber == "0" && operation != '' && currentNumber != '0') {
+      switch (operation) {     
+        case '+':
+          handleSumNumbers()
+          break
+      
+        default:
+          break
+      }
+    }
+
   }
 
 
@@ -42,14 +76,14 @@ const App = () => {
           <Button label="4" onCLick={() => handleAddNumber('4')}/>
           <Button label="5" onCLick={() => handleAddNumber('5')}/>
           <Button label="6" onCLick={() => handleAddNumber('6')}/>
-          <Button label="+" onCLick={() => handleAddNumber('+')}/>
+          <Button label="+" onCLick={handleSumNumbers}/>
         </Row>
 
         <Row>
           <Button label="1" onCLick={() => handleAddNumber('1')}/>
           <Button label="2" onCLick={() => handleAddNumber('2')}/>
           <Button label="3" onCLick={() => handleAddNumber('3')}/>
-          <Button label="=" onCLick={() => handleAddNumber('=')}/>
+          <Button label="=" onCLick={handleEquals}/>
         </Row>
 
         
